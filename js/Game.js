13308feccbee1af470b04626d4161016f32dd2bc
preetipeddu <peddupreeti@gmail.com>
2021-01-30 13:29:28 -0500

    added images to cars and track

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(carImg1);
     car2 = createSprite(300,200);
+    car2.addImage(carImg2);
     car3 = createSprite(500,200);
+    car3.addImage(carImg3);
     car4 = createSprite(700,200);
+    car4.addImage(carImg4);
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,12 +47,15 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
+      background(groundImg);
+
+      image(trackImg, 0, -displayHeight*4, displayWidth, displayHeight*5);
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -79,6 +86,14 @@ class Game {
       player.update();
     }
 
+    if(player.distance>3860){
+      gameState = 2;
+    }
+
     drawSprites();
+  }
+
+  end(){
+    console.log("Game Ended");
   }
 }
